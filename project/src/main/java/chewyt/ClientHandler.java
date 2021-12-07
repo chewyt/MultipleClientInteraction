950632974e8715c9f4579240bfb9e994d85c6abb
chewyt <yeetian.chew@gmail.com>
2021-12-07 16:46:18 +0800

    Added Server class and ClientHandler

---
+++ project/src/main/java/chewyt/ClientHandler.java
@@ -0,0 +1,92 @@
+package chewyt;
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    public static ArrayList<ClientHandler> clienthandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader br;
+    private BufferedWriter bw;
+    private String username;
+
+    public ClientHandler(Socket socket){
+
+        try {
+            this.socket = socket;
+            this.bw = new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream()));
+            this.br = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            this.username = br.readLine();
+            //Adding client to arraylist, so that it can be part of group chat
+            clienthandlers.add(this);
+            //Informing the chat that somebody joined in the session
+            broadcastMessage("[SERVER] "+ username +" has entered the chat!")
+        } catch (IOException e) {
+            closeEverything(socket,br,bw); 
+        }
+
+    }
+
+    //Running run() to use separate threads to perform for blocking operations like receiving messages
+    @Override
+    public void run() {
+        String messageFromClient ="";
+        while(socket.isConnected()){
+            try {
+
+                messageFromClient = br.readLine(); //blocking operation
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket,br,bw);
+                break;
+            }
+        }
+        
+    }
+
+    public void broadcastMessage(String message){
+
+        for(ClientHandler aclient : clienthandlers){
+            
+            try {
+                if (!aclient.username.equals(username)) {
+                    aclient.bw.write(message);
+                    aclient.bw.newLine(); //explicity write "Enter" for receiver to accept message due to br.readline()
+                    aclient.bw.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, br, bw);
+            }
+
+        }
+
+    }
+
+    public void removeClientHandler(){
+        clienthandlers.remove(this);
+        broadcastMessage("[SERVER] "+username+ " has left the chat!");
+
+    }
+
+    public void closeEverything(Socket socket, BufferedReader br, BufferedWriter bw){
+        removeClientHandler();
+        try {
+            if(br!=null){
+                br.close();
+            }
+            if (bw!=null){
+                bw.close();
+            }
+            if(socket!=null){
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ project/src/main/java/chewyt/Server.java
@@ -0,0 +1,67 @@
+package chewyt;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+    private static final int port = 12345;
+    private ServerSocket server;
+    
+    public Server(ServerSocket server){
+        this.server = server;
+    }
+    
+    public void startServer(){
+        try {
+
+            while(!server.isClosed()){
+                Socket socket = server.accept();
+                System.out.println("A new client has connected.");
+                ClientHandler clienthandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clienthandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+
+    public void closeServerSocket(){
+
+        try {
+            if(server!=null){
+                server.close();
+            }
+            
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        catch (IOException i) {
+            i.printStackTrace();
+        }
+    } 
+    
+    public static void main(String[] args) throws IOException {
+        
+        //Setup server 
+        ServerSocket serverSocket = new ServerSocket(port);
+        Server server = new Server(serverSocket);
+        server.startServer();
+
+        /* //Listening for client
+        System.out.println("[SERVER] Server ready. Listening for client connection...");
+        Socket client = server.accept();
+        System.out.println("[SERVER] Client joined the server!");
+
+        //Server needs to send message to client
+        PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+        //Server needs to read data from client socket
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));  */
+
+
+    }
+
+
+}
